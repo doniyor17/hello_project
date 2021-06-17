c4744604c8b3e46eef667250eed10b1848881282
doniyor17 <58927624+doniyor17@users.noreply.github.com>
2021-06-17 20:22:17 +0300

    basic flutter app with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
+import 'package:flutter/material.dart';
+
 void main() {
-  addNumbers(1, 6);
+  runApp(MyApp());
 }
 
-addNumbers(int num1, int num2) {
-  print(num1 + num2);
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    // var questions = [
+    //   'What\'s your favorite color?',
+    //   'What\'s your favorite animal?',
+    //   'What\'s your favorite number?'
+    // ];
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('App'),
+      ),
+      body: Column(
+        children: [
+          Text('Some text:'),
+          ElevatedButton(
+            child: Text('Press me 1'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Press me 2'),
+            onPressed: null,
+          ),
+          ElevatedButton(
+            child: Text('Press me 3'),
+            onPressed: null,
+          ),
+        ],
+      ),
+    ));
+  }
 }
